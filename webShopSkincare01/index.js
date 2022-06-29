35d37cffbf9a77c8d53e407c171b7282cc884b47
you <anusorn1039@gmail.com>
2022-06-29 12:56:27 +0700

    Edit page

--- webShopSkincare01/index.js
+++ webShopSkincare01/index.js
@@ -5,9 +5,9 @@ const app = express();
 app.use(express.urlencoded({extended : false}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine' , 'ejs');
-
+let PORT = process.env.PORT || 5565 ;
 app.get('/', (req,res) => {
-    res.render('home');
+    res.render('moblie_home');
 });
 
-app.listen(5565 ,()=> console.log("Server Start Port 5565"));
+app.listen(PORT ,()=> console.log("Server Start Port 5565"));
