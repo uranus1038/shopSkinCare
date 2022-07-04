c60a3ffca382758d3fef31061a18906c3b9437be
you <anusorn1039@gmail.com>
2022-07-04 18:15:47 +0700

    Edit Commit Folder Page

--- mdskincare/index.js
+++ mdskincare/index.js
@@ -4,12 +4,16 @@ const path = require('path');
 const app = express();
 app.use(express.urlencoded({extended : false}));
 app.use(express.static(__dirname + '/C5S'))
+app.use(express.static(__dirname + '/C5S/orderRenderCss'))
 app.use(express.static(__dirname + '/J5'))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine' , 'ejs');
 let PORT = process.env.PORT || 5565 ;
 app.get('/', (req,res) => { 
     res.render('moblieHome');
+});
+app.get('/md-skincare-cream', (req,res) => { 
+    res.render('order');
 });
 
 app.listen(PORT ,()=> console.log("Server Start Port 5565"));
