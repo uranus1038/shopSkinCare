83e6b1c8d484e02b12eb0779e2ae77d020ec7e69
you <anusorn1039@gmail.com>
2022-07-06 17:56:12 +0700

    Edit Commit Floder

--- mdskincare/index.js
+++ mdskincare/index.js
@@ -6,6 +6,7 @@ app.use(express.urlencoded({extended : false}));
 app.use(express.static(__dirname + '/C5S'))
 app.use(express.static(__dirname + '/C5S/orderRenderCss'))
 app.use(express.static(__dirname + '/J5'))
+app.use(express.static(__dirname + '/IMG'))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine' , 'ejs');
 let PORT = process.env.PORT || 5565 ;
